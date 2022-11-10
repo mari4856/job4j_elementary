ef0e1d616a55a58e46467686a896a30360eab73e
mari4856 <mari.bingo@bk.ru>
2022-11-10 23:15:58 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Studentlnfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Studentlnfo {
+    public static void main(String[] args){
+        System.out.println("Veronika");
+        System.out.println("01.12.2001");
+
+    }
+}
