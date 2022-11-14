5a473c63de72524c2167c426a87f37a49d2d9e87
mari4856 <mari.bingo@bk.ru>
2022-11-14 19:28:36 +0300

    Variable

---
+++ src/main/java/ru/job4j/Variable.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Variable {
+  public static void main(String[] args) {
+    int age = 18;
+    float length = 78.0332F;
+    boolean baby = true;
+    System.out.println(age);
+    System.out.println(length);
+    System.out.println(baby);
+  }
+}
