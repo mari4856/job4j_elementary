b9b2810f976a9f2128fda9c89ae3bae156cc2e42
mari4856 <mari.bingo@bk.ru>
2022-11-14 17:41:52 +0300

    Info

--- src/main/java/ru/job4j/Studentlnfo.java
+++ src/main/java/ru/job4j/Studentlnfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Studentlnfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Veronika");
         System.out.println("01.12.2001");
 
