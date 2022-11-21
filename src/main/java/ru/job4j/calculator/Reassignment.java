7e13c42a26a72db18c0966ba2203553e99c5b825
mari4856 <mari.bingo@bk.ru>
2022-11-21 20:13:23 +0300

    Reassignment

---
+++ src/main/java/ru/job4j/calculator/Reassignment.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculator;
+
+public class Reassignment {
+  public static void main(String[] args) {
+    int size = 10;
+    size = 100;
+    size = size - 5;
+    System.out.println(size);
+
+  }
+}
