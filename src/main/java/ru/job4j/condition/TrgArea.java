402e9789ed6a68fbe7a351901b071e689daf4512
mari4856 <mari.bingo@bk.ru>
2023-07-17 22:41:20 +0300

    TrgArea

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class TrgArea {
   public static double area(double a, double b, double c) {
     double q = (a + b + c) / 2;
-    double s = Math.sqrt(q * ((q - a) * (q - b) * (q - c)));
+    double s = Math.sqrt(q * (q - a) * (q - b) * (q - c));
     return s;
   }
 
