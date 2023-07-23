a925dee44a3080a8f06cb2c245232ecba475070c
mari4856 <mari.bingo@bk.ru>
2023-07-23 21:42:15 +0300

    Converter

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,19 +3,25 @@ package ru.job4j.converter;
 public class Converter {
 
   public static float rubleToEuro(float value) {
-    float rsl = value / 70;
+    float rsl = value / 100;
     return rsl;
   }
 
   public static float rubleToDollar(float value) {
-    float rsl = value / 60;
+    float rsl = value / 90;
     return rsl;
   }
 
   public static void main(String[] args) {
-    float dollar = Converter.rubleToDollar(60);
-    float euro = Converter.rubleToEuro(70);
-    System.out.println("70 rubles are " + euro + " euro.");
-    System.out.println("60 rubles are " + dollar + " euro.");
+    float inOne = 200;
+    float inTwo = 180;
+    float expectedOne = 2;
+    float expectedTwo = 2;
+    float outOne = Converter.rubleToEuro(inOne);
+    float outTwo = Converter.rubleToDollar(inTwo);
+    boolean passedOne = expectedOne == outOne;
+    boolean passedTwo = expectedTwo == outTwo;
+    System.out.println("200 rubles are 2. Test result : " + passedOne);
+    System.out.println("180 rubles are 2. Test result : " + passedTwo);
   }
 }
